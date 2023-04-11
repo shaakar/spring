4c18881e3fef76ede3d850d374c6d8886825b567
shaakar <129154651+shaakar@users.noreply.github.com>
2023-04-11 13:29:05 +0530

    first ocmmit

---
+++ demo-github/src/test/java/com/githubs/demogithub/DemoGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubs.demogithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
